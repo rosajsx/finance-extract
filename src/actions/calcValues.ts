6fcd6dbbfab7670b7a5b366424cd5859e2d56396
Lucas Rosa <lucassantosrosa51@gmail.com>
2025-07-06 10:03:43 -0300

    feat: implement calcValues function to aggregate financial data from records

---
+++ src/actions/calcValues.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { ExtractRecord } from "@/types";
+
+export type Item = {
+  label: string;
+  value: number;
+};
+
+export interface CalcValues {
+  [key: string]: Item;
+}
+
+export async function calcValues(data: ExtractRecord[]) {
+  "use server";
+
+  const result = data.reduce(
+    (acc, record) => {
+      if (
+        record["Lançamento"].includes("PAGAMENTO") ||
+        record["Lançamento"].includes("AMORTIZAÇÃO")
+      ) {
+        console.log("a");
+        return acc;
+      }
+
+      const value = parseFloat(
+        record.Valor.replace("R$", "").replace(".", "").replace(",", ".").trim()
+      );
+
+      acc.total.value += isNaN(value) ? 0 : value;
+
+      if (!acc[record.Categoria]) {
+        acc[record.Categoria] = {
+          label: record.Categoria,
+          value,
+        };
+      } else {
+        acc[record.Categoria].label = record.Categoria;
+        acc[record.Categoria].value += value;
+      }
+
+      return acc;
+    },
+    {
+      total: { label: "Total", value: 0 },
+    } as CalcValues
+  );
+
+  return result;
+}
